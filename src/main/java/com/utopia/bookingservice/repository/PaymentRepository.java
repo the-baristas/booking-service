b18705aae5fcb288a2dabfdacc5e186efea0b80e
Charles Kim <charles.kim@smoothstack.com>
2021-06-16 22:02:42 -0400

    Add endpoint to delete a payment and to find a payment by the stripe ID, fix controller advice to only handle SQL exceptions

--- src/main/java/com/utopia/bookingservice/repository/PaymentRepository.java
+++ src/main/java/com/utopia/bookingservice/repository/PaymentRepository.java
@@ -1,8 +1,12 @@
 package com.utopia.bookingservice.repository;
 
+import java.util.Optional;
+
 import com.utopia.bookingservice.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByStripeId(String stripeId);
 
+    void deleteByStripeId(String stripeId);
 }
